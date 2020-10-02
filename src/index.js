f38bb53f277d11b75483fbb3e9a78f7e78c9829c
Al-Abramov <x3m.f@mail.ru>
2020-10-03 00:24:34 +0400

    feat: add functiom mim, max, avg from array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length) ? array.sort((a,b)=>(a-b)).reverse().pop(0) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length) ? array && array.sort((a,b)=>(a-b)).pop(0) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length) ? array.reduce((a,b)=>(a+b))/array.length : 0;
 }
+
