69ee067ade94955043cbc498cf5972931085113a
Alex <x3m.f@mail.ru>
2021-03-03 13:04:59 +0400

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 exports.min = function min (array) {
-  return (array && array.length) ? array.sort((a,b)=>(a-b)).reverse().pop(0) : 0;
+    return (array && array.length) ? Math.min(...array) : 0;
 }
 
+
 exports.max = function max (array) {
-  return (array && array.length) ? array && array.sort((a,b)=>(a-b)).pop(0) : 0;
+    return (array && array.length) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
